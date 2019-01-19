b06b3b32edc613105f457ba93ba2f7c2d8122c7b
Natallia Lastavenka <n.lastavenka@godeltech.com>
2019-01-19 22:39:10 +0300

    Updates

---
+++ precache-manifest.825cb08f76c38a11c30a61b35adf5282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f400959a05aa3fe7377",
+    "url": "/playlist/static/js/runtime~main.4f400959.js"
+  },
+  {
+    "revision": "d3fd0921b1acf7d04ba3",
+    "url": "/playlist/static/js/main.d3fd0921.chunk.js"
+  },
+  {
+    "revision": "3a85f18a8320fed46777",
+    "url": "/playlist/static/js/1.3a85f18a.chunk.js"
+  },
+  {
+    "revision": "d3fd0921b1acf7d04ba3",
+    "url": "/playlist/static/css/main.e5e0f557.chunk.css"
+  },
+  {
+    "revision": "5b32996905eccdf71d8d737f9a4a3b5d",
+    "url": "/playlist/index.html"
+  }
+];
