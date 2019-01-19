9d97aa4515c6c9f3e2c6e8dc9170196e4b0b3a14
Natallia Lastavenka <n.lastavenka@godeltech.com>
2019-01-19 11:07:40 +0300

    Constants and helpers added

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const GENRES_COUNT = 10;
+const SONGS_COUNT = 200;
+const BANDS_COUNT = 20;
+const YEAR_MIN = 1986;
+const YEAR_MAX = 2019;
+
+const HEADERS = ["Исполнитель", "Песня", "Жанр", "Год"];
+
+export {
+  GENRES_COUNT,
+  SONGS_COUNT,
+  BANDS_COUNT,
+  YEAR_MIN,
+  YEAR_MAX,
+  HEADERS
+};
